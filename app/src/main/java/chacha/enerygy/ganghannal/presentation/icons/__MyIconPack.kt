bb9351759cbbd1b6e34ade10bc673e396ebe6270
이정은 <silver36c@gmail.com>
2024-07-18 07:52:08 +0900

    💄 Design: EcgHeart 아이콘 추가

---
+++ app/src/main/java/chacha/enerygy/ganghannal/presentation/icons/__MyIconPack.kt
@@ -0,0 +1,18 @@
+package chacha.enerygy.ganghannal.presentation.icons
+
+import androidx.compose.ui.graphics.vector.ImageVector
+import chacha.enerygy.ganghannal.presentation.icons.myiconpack.EcgHeart
+import kotlin.collections.List as ____KtList
+
+object MyIconPack
+
+private var __AllIcons: ____KtList<ImageVector>? = null
+
+val MyIconPack.AllIcons: ____KtList<ImageVector>
+    get() {
+        if (__AllIcons != null) {
+            return __AllIcons!!
+        }
+        __AllIcons = listOf(EcgHeart)
+        return __AllIcons!!
+    }
---
+++ app/src/main/java/chacha/enerygy/ganghannal/presentation/icons/myiconpack/EcgHeart.kt
@@ -0,0 +1,107 @@
+package chacha.enerygy.ganghannal.presentation.icons.myiconpack
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PathFillType.Companion.NonZero
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.graphics.StrokeCap.Companion.Butt
+import androidx.compose.ui.graphics.StrokeJoin.Companion.Miter
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.vector.ImageVector.Builder
+import androidx.compose.ui.graphics.vector.path
+import androidx.compose.ui.unit.dp
+import chacha.enerygy.ganghannal.presentation.icons.MyIconPack
+
+val MyIconPack.EcgHeart: ImageVector
+    get() {
+        if (_ecgHeart24dp5f6368Fill0Wght400Grad0Opsz24 != null) {
+            return _ecgHeart24dp5f6368Fill0Wght400Grad0Opsz24!!
+        }
+        _ecgHeart24dp5f6368Fill0Wght400Grad0Opsz24 = Builder(
+            name =
+            "EcgHeart24dp5f6368Fill0Wght400Grad0Opsz24", defaultWidth = 24.0.dp, defaultHeight =
+            24.0.dp, viewportWidth = 960.0f, viewportHeight = 960.0f
+        ).apply {
+            path(
+                fill = SolidColor(Color(0xFF5f6368)), stroke = null, strokeLineWidth = 0.0f,
+                strokeLineCap = Butt, strokeLineJoin = Miter, strokeLineMiter = 4.0f,
+                pathFillType = NonZero
+            ) {
+                moveTo(480.0f, 480.0f)
+                close()
+                moveTo(480.0f, 840.0f)
+                quadToRelative(-18.0f, 0.0f, -34.5f, -6.5f)
+                reflectiveQuadTo(416.0f, 814.0f)
+                lineTo(148.0f, 545.0f)
+                quadToRelative(-35.0f, -35.0f, -51.5f, -80.0f)
+                reflectiveQuadTo(80.0f, 371.0f)
+                quadToRelative(0.0f, -103.0f, 67.0f, -177.0f)
+                reflectiveQuadToRelative(167.0f, -74.0f)
+                quadToRelative(48.0f, 0.0f, 90.5f, 19.0f)
+                reflectiveQuadToRelative(75.5f, 53.0f)
+                quadToRelative(32.0f, -34.0f, 74.5f, -53.0f)
+                reflectiveQuadToRelative(90.5f, -19.0f)
+                quadToRelative(100.0f, 0.0f, 167.5f, 74.0f)
+                reflectiveQuadTo(880.0f, 370.0f)
+                quadToRelative(0.0f, 49.0f, -17.0f, 94.0f)
+                reflectiveQuadToRelative(-51.0f, 80.0f)
+                lineTo(543.0f, 814.0f)
+                quadToRelative(-13.0f, 13.0f, -29.0f, 19.5f)
+                reflectiveQuadToRelative(-34.0f, 6.5f)
+                close()
+                moveTo(520.0f, 320.0f)
+                quadToRelative(10.0f, 0.0f, 19.0f, 5.0f)
+                reflectiveQuadToRelative(14.0f, 13.0f)
+                lineToRelative(68.0f, 102.0f)
+                horizontalLineToRelative(166.0f)
+                quadToRelative(7.0f, -17.0f, 10.5f, -34.5f)
+                reflectiveQuadTo(801.0f, 370.0f)
+                quadToRelative(-2.0f, -69.0f, -46.0f, -118.5f)
+                reflectiveQuadTo(645.0f, 202.0f)
+                quadToRelative(-31.0f, 0.0f, -59.5f, 12.0f)
+                reflectiveQuadTo(536.0f, 249.0f)
+                lineToRelative(-27.0f, 29.0f)
+                quadToRelative(-5.0f, 6.0f, -13.0f, 9.5f)
+                reflectiveQuadToRelative(-16.0f, 3.5f)
+                quadToRelative(-8.0f, 0.0f, -16.0f, -3.5f)
+                reflectiveQuadToRelative(-14.0f, -9.5f)
+                lineToRelative(-27.0f, -29.0f)
+                quadToRelative(-21.0f, -23.0f, -49.0f, -36.0f)
+                reflectiveQuadToRelative(-60.0f, -13.0f)
+                quadToRelative(-66.0f, 0.0f, -110.0f, 50.5f)
+                reflectiveQuadTo(160.0f, 370.0f)
+                quadToRelative(0.0f, 18.0f, 3.0f, 35.5f)
+                reflectiveQuadToRelative(10.0f, 34.5f)
+                horizontalLineToRelative(187.0f)
+                quadToRelative(10.0f, 0.0f, 19.0f, 5.0f)
+                reflectiveQuadToRelative(14.0f, 13.0f)
+                lineToRelative(35.0f, 52.0f)
+                lineToRelative(54.0f, -162.0f)
+                quadToRelative(4.0f, -12.0f, 14.5f, -20.0f)
+                reflectiveQuadToRelative(23.5f, -8.0f)
+                close()
+                moveTo(532.0f, 450.0f)
+                lineTo(478.0f, 612.0f)
+                quadToRelative(-4.0f, 12.0f, -15.0f, 20.0f)
+                reflectiveQuadToRelative(-24.0f, 8.0f)
+                quadToRelative(-10.0f, 0.0f, -19.0f, -5.0f)
+                reflectiveQuadToRelative(-14.0f, -13.0f)
+                lineToRelative(-68.0f, -102.0f)
+                lineTo(236.0f, 520.0f)
+                lineToRelative(237.0f, 237.0f)
+                quadToRelative(2.0f, 2.0f, 3.5f, 2.5f)
+                reflectiveQuadToRelative(3.5f, 0.5f)
+                quadToRelative(2.0f, 0.0f, 3.5f, -0.5f)
+                reflectiveQuadToRelative(3.5f, -2.5f)
+                lineToRelative(236.0f, -237.0f)
+                lineTo(600.0f, 520.0f)
+                quadToRelative(-10.0f, 0.0f, -19.0f, -5.0f)
+                reflectiveQuadToRelative(-15.0f, -13.0f)
+                lineToRelative(-34.0f, -52.0f)
+                close()
+            }
+        }
+            .build()
+        return _ecgHeart24dp5f6368Fill0Wght400Grad0Opsz24!!
+    }
+
+private var _ecgHeart24dp5f6368Fill0Wght400Grad0Opsz24: ImageVector? = null
