f1f3f25cbf3c42680241c67d0082fb75e7047487
이정은 <silver36c@gmail.com>
2024-07-18 07:50:36 +0900

    💄 Design: 앱 컬러 추가

---
+++ app/src/main/java/chacha/enerygy/ganghannal/presentation/theme/AppColor.kt
@@ -0,0 +1,19 @@
+package chacha.enerygy.ganghannal.presentation.theme
+
+import androidx.compose.ui.graphics.Color
+
+enum class AppColor(val color: Color) {
+    //    primary(Color(0xFF5B7BCE)),
+    primary(Color(0xFF5B7BCE)),
+    onPrimary(Color(0xFF1a2e60)),
+    secondary(Color(0xFFF74C4C)),
+    onSecondary(Color(0xFF571e1c)),
+    secondaryContainer(Color(0xFFFDD2D2)),
+    onSecondaryContainer(Color(0xFF3b0809)),
+    background(Color(0xFF111318)),
+    surface(Color(0xFF303133)),
+    onSurface(Color(0xFFDADCE0)),
+    textBlack(Color(0xFF2E4661)),
+    textGray(Color(0xFFA5A5A5)),
+    textWhite(Color(0xFFf9f9ff))
+}
