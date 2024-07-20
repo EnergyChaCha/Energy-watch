573dc860e21ce2d724846bb2dff009bdc2dbec3c
이정은 <silver36c@gmail.com>
2024-07-20 17:29:45 +0900

    🔧 Chore: 건강 관리 서비스 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,4 +70,14 @@ dependencies {
 
     implementation("androidx.navigation:navigation-compose:2.7.7")
     implementation("androidx.paging:paging-compose:3.3.0")
+
+    // 건강 관리 서비스
+    implementation("androidx.health:health-services-client:1.1.0-alpha03")
+
+    // ListenableFuture: 비동기 처리
+    implementation("com.google.guava:guava:31.0.1-android")
+    // To use CallbackToFutureAdapter
+    implementation("androidx.concurrent:concurrent-futures:1.2.0")
+    // Kotlin
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-guava:1.6.0")
 }
